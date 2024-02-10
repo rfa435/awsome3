49caa4b1d197582f0dc2d260603cfaad8b1f7226
rfabhian <fabhian@amazon.com>
2024-02-11 06:15:35 +0700

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 80;
 
 const sql = require('mssql');
 
